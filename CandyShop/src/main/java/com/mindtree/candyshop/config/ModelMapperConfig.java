c877eb807d76d7bbb7e48f83e1d73f970f533580
karthiktr147 <61529403+karthiktr147@users.noreply.github.com>
2020-05-05 20:10:11 +0530

    Update ModelMapperConfig.java

--- CandyShop/src/main/java/com/mindtree/candyshop/config/ModelMapperConfig.java
+++ CandyShop/src/main/java/com/mindtree/candyshop/config/ModelMapperConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 //conflict
 //rise
+//ktr
 @Configuration
 public class ModelMapperConfig {
 
