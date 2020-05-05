3f1d646ab3568b84508fee556d5f6106f84afb08
karthiktr147 <karthiktr147@gmail.com>
2020-05-05 19:45:04 +0530

    "second commit feature"

--- CandyShop/src/main/java/com/mindtree/candyshop/config/SwaggerConfig.java
+++ CandyShop/src/main/java/com/mindtree/candyshop/config/SwaggerConfig.java
@@ -14,7 +14,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 public class SwaggerConfig {
 
  
-
+//feature
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2).select().apis(RequestHandlerSelectors.any())
